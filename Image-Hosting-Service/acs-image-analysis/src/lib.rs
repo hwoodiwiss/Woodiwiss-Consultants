4eb5d60002171d4b06b7daa7627eb6f42adc57d6
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-12-01 22:23:48 +0000

    Start adding library docs

--- Image-Hosting-Service/acs-image-analysis/src/lib.rs
+++ Image-Hosting-Service/acs-image-analysis/src/lib.rs
@@ -11,13 +11,27 @@ use internal::AzureImageAnalysisClientInternal;
 #[macro_use]
 extern crate serde;
 
+/// A safe Azure Cognitive Services Image Analysis API client.
+///
+/// Used to interact with an Azure Cognitive Services instance and provides
+/// access to the Image Analysis APIs.
 pub struct AzureImageAnalysisClient(AzureImageAnalysisClientInternal);
 
 impl AzureImageAnalysisClient {
+    /// Instantiates a new AzureImageAnalysisClient
+    /// `base_url` is the URL of the Azure Cognitive Services instance
+    /// `key` is the API key to use for authentication
     pub fn new(base_uri: &str, key: &str) -> Self {
         AzureImageAnalysisClient(AzureImageAnalysisClientInternal::new(base_uri, key))
     }
 
+    /// Analyses an image passed as a byte array.
+    /// Returns the analysis result returned from Azure.
+    ///
+    ///  # Errors
+    ///
+    /// If something goes wrong, the Error will be returned. This incudes `HttpError`'s
+    /// which will be wrapped in `ImageAnalysisError::HttpError`.
     pub async fn analyse(&self, image_data: Vec<u8>) -> Result<ImageAnalysis, ImageAnalysisError> {
         let http_client = DirectHttpClient(reqwest::Client::new());
         self.0.analyse(&http_client, image_data).await
