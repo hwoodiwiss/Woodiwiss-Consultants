e96c8aea79184169c4f84f50028ffd00c753e0a3
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-12-03 22:06:58 +0000

    Add ability for endpoints to self-manage mounting routes

--- Image-Hosting-Service/image-hosting-service/src/endpoints/home.rs
+++ Image-Hosting-Service/image-hosting-service/src/endpoints/home.rs
@@ -1,3 +1,12 @@
+use rocket::{Build, Rocket};
+
+use crate::responders::OptionsResponse;
+
+pub fn mount(rocket: Rocket<Build>) -> Rocket<Build> {
+    let rocket = rocket.mount("/", routes![options, index]);
+    rocket
+}
+
 #[options("/")]
 pub fn options() -> OptionsResponse {
     OptionsResponse {
