75e226e5d510a0fd3cae5a481cae559f02508383
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-12-10 23:16:48 +0000

    Adjust Credential interception to only add creds for cms

--- Woodiwiss-Consultants-Web/src/app/services/CredentialsInterceptor.ts
+++ Woodiwiss-Consultants-Web/src/app/services/CredentialsInterceptor.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 /** Inject With Credentials into the request */
 @Injectable()
 export class CredentialsInterceptor implements HttpInterceptor {
 	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 		// console.log("interceptor: " + req.url);
-		req = req.clone({
-			withCredentials: true,
-		});
+		if (req.url.startsWith(environment.cmsApiUri)) {
+			req = req.clone({
+				withCredentials: true,
+			});
+		}
 
 		return next.handle(req);
 	}
