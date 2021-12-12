ddddb6ef4a5401d07d697928be8a6f81bbf59d9d
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-12-12 22:17:18 +0000

    Rename image hosting module

--- Woodiwiss-Consultants-Web/src/app/image-hosting/image-hosting.module.ts
+++ Woodiwiss-Consultants-Web/src/app/image-hosting/image-hosting.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GalleryPageComponent } from './image-hosting.component';
 import { SharedModule } from '../shared/shared.module';
+import { AddImageFormModule } from '../shared/add-image-form/add-image-form.module';
 
 @NgModule({
 	declarations: [GalleryPageComponent],
-	imports: [CommonModule, SharedModule],
+	imports: [CommonModule, SharedModule, AddImageFormModule],
 })
-export class GalleryPageModule {}
+export class ImageHostingPageModule {}
