48a971529c06b4622aaf537501cc7a0a93c54adf
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-12-12 00:16:39 +0000

    Add http test module to consumers of image service

--- Woodiwiss-Consultants-Web/src/app/image-hosting/image-hosting.component.spec.ts
+++ Woodiwiss-Consultants-Web/src/app/image-hosting/image-hosting.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SharedTestingModule } from '../shared/shared-testing.module';
 
@@ -10,7 +11,7 @@ describe('GalleryPageComponent', () => {
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			declarations: [GalleryPageComponent],
-			imports: [SharedTestingModule],
+			imports: [SharedTestingModule, HttpClientTestingModule],
 		}).compileComponents();
 	});
 
