963742cc1bbc8bf87049350ca54e3a00bc3185b5
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-12-13 16:01:33 +0000

    Add db storage for filetype

---
+++ Image-Hosting-Service/image-hosting-service/migrations/2021-12-13-155050_add_image_filetype/down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `Images` DROP `FileType`;
--- Image-Hosting-Service/image-hosting-service/src/database/mod.rs
+++ Image-Hosting-Service/image-hosting-service/src/database/mod.rs
@@ -14,6 +14,7 @@ table! {
         ImageData -> Longtext,
         Description -> Char,
         Hidden -> Bool,
+        FileType -> Varchar,
     }
 }
 
