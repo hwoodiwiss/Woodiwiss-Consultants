a52e5719d2d327fe63fb0c59a5cf4f604985e493
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-12-13 14:24:11 +0000

    Remove services and about page

--- Woodiwiss-Consultants-Web/src/app/app.module.ts
+++ Woodiwiss-Consultants-Web/src/app/app.module.ts
@@ -5,11 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { ContactPageModule } from './contact-page/contact-page.module';
-import { AboutPageModule } from './about-page/about-page.module';
 import { AdminPageModule } from './admin-page/admin-page.module';
 import { HomePageModule } from './home-page/home-page.module';
 import { NotFoundModule as NotFoundPageModule } from './not-found-page/not-found-page.module';
-import { ServicesPageModule } from './services-page/services-page.module';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CredentialsInterceptor } from './services/CredentialsInterceptor';
 import { LoginPageModule } from './login-page/login-page.module';
@@ -23,14 +21,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 		AppRoutingModule,
 		SharedModule,
 		ContactPageModule,
-		AboutPageModule,
 		AdminPageModule,
 		HomePageModule,
 		NotFoundPageModule,
-		ServicesPageModule,
 		LoginPageModule,
 		ImageHostingPageModule,
-  FontAwesomeModule,
+		FontAwesomeModule,
 	],
 	providers: [{ provide: HTTP_INTERCEPTORS, useClass: CredentialsInterceptor, multi: true }],
 
