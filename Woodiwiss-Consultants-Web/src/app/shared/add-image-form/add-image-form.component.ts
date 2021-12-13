9d6333fe0494a1af253e0967b4d9cfa1f75f443b
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-12-13 02:37:52 +0000

    Fill out form some more Add preview

--- Woodiwiss-Consultants-Web/src/app/shared/add-image-form/add-image-form.component.ts
+++ Woodiwiss-Consultants-Web/src/app/shared/add-image-form/add-image-form.component.ts
@@ -1,10 +1,11 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ReCaptchaV3Service } from 'ng-recaptcha';
 import { UsersApiService } from 'src/app/services/api/usersApi.service';
 import { NewUserFormModel } from 'src/app/services/api/UsersApiModels';
 import { ImageService } from 'src/app/services/image.service';
 import { FormBaseComponent } from '../form-base/form-base.component';
+import { faCloudUploadAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
 	selector: 'wcw-add-image-form',
@@ -12,6 +13,12 @@ import { FormBaseComponent } from '../form-base/form-base.component';
 	styleUrls: ['./add-image-form.component.scss'],
 })
 export class AddImageFormComponent extends FormBaseComponent {
+	faCloudUpload = faCloudUploadAlt;
+	imgSource: string | null = null;
+	invalidImage: boolean = false;
+	@ViewChild('canvas', { static: true })
+	canvas: ElementRef<HTMLCanvasElement>;
+
 	constructor(recaptcha: ReCaptchaV3Service, private imageService: ImageService) {
 		super(recaptcha, 'addImageForm', true);
 
@@ -22,11 +29,24 @@ export class AddImageFormComponent extends FormBaseComponent {
 
 	onFileChanged(event) {
 		if (event.target.files.length > 0) {
-			const file = event.target.files[0];
+			const file = event.target.files[0] as File;
+			this.invalidImage = false;
+			let fr = new FileReader();
+			fr.onload = this.setImageValue.bind(this);
+			fr.readAsDataURL(file);
 			this.formGroup.patchValue({
 				ImageFile: file,
 			});
 		}
+	}
+
+	setImageValue(progressEvt: ProgressEvent<FileReader>) {
+		this.imgSource = progressEvt.target.result as string;
+	}
+
+	onImageLoadFailed() {
+		this.imgSource = null;
+		this.invalidImage = true;
 	}
 
 	public async submitData(token: string) {
