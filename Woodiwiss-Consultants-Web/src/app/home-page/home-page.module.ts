5d2d7ddbc96f3ce69fb60f4927770c9ab98e7520
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-12-13 18:18:08 +0000

    Move new image form to home

--- Woodiwiss-Consultants-Web/src/app/home-page/home-page.module.ts
+++ Woodiwiss-Consultants-Web/src/app/home-page/home-page.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { SharedModule } from '../shared/shared.module';
+import { AddImageFormModule } from '../shared/add-image-form/add-image-form.module';
 
 @NgModule({
 	declarations: [HomePageComponent],
-	imports: [CommonModule, SharedModule],
+	imports: [CommonModule, SharedModule, AddImageFormModule],
 })
 export class HomePageModule {}
--- Woodiwiss-Consultants-Web/src/app/image-hosting/image-hosting.module.ts
+++ Woodiwiss-Consultants-Web/src/app/image-hosting/image-hosting.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageHostingPage } from './image-hosting.component';
 import { SharedModule } from '../shared/shared.module';
-import { AddImageFormModule } from '../shared/add-image-form/add-image-form.module';
 
 @NgModule({
 	declarations: [ImageHostingPage],
-	imports: [CommonModule, SharedModule, AddImageFormModule],
+	imports: [CommonModule, SharedModule],
 })
 export class ImageHostingPageModule {}
