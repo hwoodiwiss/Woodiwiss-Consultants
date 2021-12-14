9869e817df0ace7cefce553a0c7d141e70e67929
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-12-14 21:57:34 +0000

    Add doc comments for OptionsResponse

--- Image-Hosting-Service/image-hosting-service/src/responders/mod.rs
+++ Image-Hosting-Service/image-hosting-service/src/responders/mod.rs
@@ -1,10 +1,25 @@
 use rocket::{http::Header, response::Responder, Response};
 
+/// A responder struct for responding to options requests
+///
+/// `allowed_methods`: A list of methods allowed by the
+/// related endpoint
 pub struct OptionsResponse {
     pub allowed_methods: Vec<&'static str>,
 }
 
 impl<'r> Responder<'r, 'static> for OptionsResponse {
+    /// Sets pre-flight CORS headers for an options request
+    ///
+    /// ## Headers
+    ///
+    /// ### Access-Control-Allow-Headers
+    /// Sets Access-Control-Allow-Headers header value to value of incoming
+    /// Access-Control-Request-Headers header, plus 'Access-Control-Allow-Origin'
+    ///
+    /// ### Access-Control-Allow-Methods
+    /// Sets Access-Control-Allow-Methods to the value off `allowed_methods` as
+    /// a comma separated list
     fn respond_to(self, request: &'r rocket::Request<'_>) -> rocket::response::Result<'static> {
         let mut requested_headers = request
             .headers()
